348ace8ddbc247ce4729d16caefbcd306a7e76a4
malaicarot <64959076+malaicarot@users.noreply.github.com>
2023-07-30 20:48:27 +0700

    add UI Templates Page

---
+++ src/utils/backgroundThemesIconData.js
@@ -0,0 +1,32 @@
+const bckThemesIconData = [
+    {
+        title: 'Business',
+        img: 'https://trello.com/assets/3919e0fe0976de0298b4.svg'
+    },
+    {
+        title: 'Design',  
+        img: 'https://trello.com/assets/157c58403db677619bea.svg'
+    },
+    {
+        title: 'Education',
+        img: 'https://trello.com/assets/dea39a045ff4c4d1d9b1.svg'
+    },
+    {
+        title: 'Technique',
+        img: 'https://trello.com/assets/e0b1e866de40a5468aaa.svg'
+    },
+    {
+        title: 'Marketing',
+        img: 'https://trello.com/assets/757286ddcfc9b405911a.svg'
+    },
+    {
+        title: 'Project Management',
+        img: 'https://trello.com/assets/7d80b8fac2bac70d9e72.svg'
+    },
+    {
+        title: 'Working Remotely',
+        img: 'https://trello.com/assets/f03ecb89626f72d4a0aa.svg'
+    },
+]
+
+export default bckThemesIconData;
