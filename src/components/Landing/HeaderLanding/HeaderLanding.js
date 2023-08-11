3fd87fe6a049049def70565b7a7759d41d85f8a0
malaicarot <64959076+malaicarot@users.noreply.github.com>
2023-08-11 15:35:58 +0700

    update responsive lading page (max-width 990px)

--- src/components/Landing/HeaderLanding/HeaderLanding.js
+++ src/components/Landing/HeaderLanding/HeaderLanding.js
@@ -1,39 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './HeaderLanding.module.scss';
 import { titleNavHeaderPage } from '../../../utils/imgHeroLandingPage';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../contexts/AuthContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 let cx = classNames.bind(styles);
 const Header = () => {
   const { currentUser } = useAuth();
   const hasCurrentUser = !!currentUser;
+
+  const [isActive, setIsActive] = useState(false);
+  const toggleActiveClass = () => {
+    setIsActive(!isActive);
+  };
+  const removeActive = () => {
+    setIsActive(false);
+  };
+
   return (
     <div className={cx('header')}>
-      <div className={cx('logo')}>
-        <img width="auto" height="40" src="/assets/logo.png" alt="logo" />
-      </div>
-      {titleNavHeaderPage.map((item) => {
-        return (
-          <nav className={cx('navbar navbar-expand-lg bg-body')}>
-            <div className="container-fluid">
-              <a className="navbar-brand" href="#"></a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon" />
+      <nav className={cx(`${styles.navbar}`)}>
+        {/* logo */}
+        <a href="#home" className={cx(`${styles.logo}`)}>
+          <img width="auto" height="40" src="/assets/logo.png" alt="logo" />
+        </a>
+
+        <ul
+          className={cx(`${styles.navMenu} ${isActive ? styles.active : ''}`)}
+        >
+          {titleNavHeaderPage.map((item) => {
+            return (
+              <li onClick={removeActive}>
+                <a href="#home" className={cx(`${styles.navLink}`)}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <li onClick={removeActive}>
+            <Link to="/u/home">
+              <button>
+                {!hasCurrentUser ? 'Đăng ký ngay' : 'Mở bảng của bạn'}
               </button>
-              <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                  <li className="nav-item dropdown">
+            </Link>
+          </li>
+        </ul>
+
+        <div
+          className={cx(`${styles.hamburger} ${isActive ? styles.active : ''}`)}
+          onClick={toggleActiveClass}
+        >
+          <span className={cx(`${styles.bar}`)}></span>
+          <span className={cx(`${styles.bar}`)}></span>
+          <span className={cx(`${styles.bar}`)}></span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+export default Header;
+{
+  /* <nav className={cx('navbar navbar-expand-lg navbar-light bg-light')}>
+        <div className={cx('container-fluid')}>
+          <a className="navbar-brand" href="#"></a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+                  <span class="navbar-toggler-icon"></span>
+
+          </button>
+          <div
+            className={cx('collapse navbar-collapse')}
+            id="navbarNavDropdown"
+          >
+            {titleNavHeaderPage.map((item) => {
+              return (
+                <ul className={cx("navbar-nav")}>
+                  <li className={cx("nav-item dropdown")}>
                     <a
-                      className="nav-link dropdown-toggle"
+                      className={cx("nav-link dropdown-toggle")}
                       href="#"
                       role="button"
                       data-bs-toggle="dropdown"
@@ -41,7 +94,7 @@ const Header = () => {
                     >
                       {item.title}
                     </a>
-                    <ul className="dropdown-menu">
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                       <li>
                         <a className="dropdown-item" href="#">
                           {item.detail1}
@@ -60,19 +113,16 @@ const Header = () => {
                     </ul>
                   </li>
                 </ul>
-              </div>
+              );
+            })}
+            <div className={cx('btnAccount')}>
+              <Link to="/u/home">
+                <button>
+                  {!hasCurrentUser ? 'Đăng ký ngay' : 'Mở bảng của bạn'}
+                </button>
+              </Link>
             </div>
-          </nav>
-        );
-      })}
-      <div className={cx('btnAccount')}>
-        <Link to="/u/home">
-          <button>
-            {!hasCurrentUser ? 'Đăng ký ngay' : 'Mở bảng của bạn'}
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-};
-export default Header;
+          </div>
+        </div>
+      </nav> */
+}
--- src/pages/Templates/Templates.js
+++ src/pages/Templates/Templates.js
@@ -1,18 +1,37 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import classNames from "classnames/bind";
 import styles from "./Templates.module.scss";
 import bckThemesIconData from "../../utils/backgroundThemesIconData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  boardsListStorage,
+  columnsListStorage,
+  cardsListStorage,
+} from "../../utils/local-storage";
 import SidebarLayout from "../../layouts/SidebarLayout/SidebarLayout";
-import ThemeContext from "../../contexts/ThemeContext";
 let cx = classNames.bind(styles);
 const Templates = () => {
-  const { setCurrentImg } = useContext(ThemeContext);
-  // Prev Templates
-  const handleGetTemplateByType = (img) => {
-    setCurrentImg(img);
+  const handleGetTemplate = (templateData) => {
+    const boardId = templateData.board.boardId;
+    const boardListData = boardsListStorage.load();
+    const shouldAdd = !boardListData.some((board) => boardId === board.boardId);
+    if (shouldAdd) {
+      // Update board data
+      const newBoardsListData = [...boardListData, templateData.board];
+      boardsListStorage.save(newBoardsListData);
+
+      // update columns data
+      const columnsListData = columnsListStorage.load();
+      const newColumnsListData = [...columnsListData, ...templateData.columns];
+      columnsListStorage.save(newColumnsListData);
+
+      // update cards data
+      const cardsListData = cardsListStorage.load();
+      const newCardsListData = [...cardsListData, ...templateData.cards];
+      cardsListStorage.save(newCardsListData);
+    }
   };
   return (
     <SidebarLayout>
@@ -29,10 +48,8 @@ const Templates = () => {
             {bckThemesIconData.map((item) => {
               return (
                 <Link
-                  to={`/templates/preview/${item.data.board.boardId}`}
-                  onClick={() =>
-                    handleGetTemplateByType(item.data.board.boardImageBg)
-                  }
+                  to={`/b/${item.data.board.boardId}`}
+                  onClick={() => handleGetTemplate(item.data)}
                 >
                   <li>
                     <img src={item.img} alt={item.title} />
