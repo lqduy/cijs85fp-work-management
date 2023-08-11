223c15b01002f38c87ea885a5611b0f899b98b3a
lqduycp <lqduycp@gmail.com>
2023-08-11 16:49:33 +0700

    done all

--- src/pages/Templates/Templates.js
+++ src/pages/Templates/Templates.js
@@ -1,18 +1,22 @@
-import React from "react";
-import classNames from "classnames/bind";
-import styles from "./Templates.module.scss";
-import bckThemesIconData from "../../utils/backgroundThemesIconData";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Templates.module.scss';
+import bckThemesIconData from '../../utils/backgroundThemesIconData';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import SidebarLayout from '../../layouts/SidebarLayout/SidebarLayout';
 import {
   boardsListStorage,
-  columnsListStorage,
   cardsListStorage,
-} from "../../utils/local-storage";
-import SidebarLayout from "../../layouts/SidebarLayout/SidebarLayout";
+  columnsListStorage,
+} from '../../utils/local-storage';
+import ThemeContext from '../../contexts/ThemeContext';
 let cx = classNames.bind(styles);
+
 const Templates = () => {
+  const { sidebarOpen } = useContext(ThemeContext);
+
   const handleGetTemplate = (templateData) => {
     const boardId = templateData.board.boardId;
     const boardListData = boardsListStorage.load();
@@ -35,30 +39,33 @@ const Templates = () => {
   };
   return (
     <SidebarLayout>
-      <div className={cx("wrapper")}>
-        <div className={cx("theme-top")}>
-          <div className={cx("theme-title")}>
-            <p>Featured categories</p>
-            <form className={cx("theme-form")}>
-              <input placeholder={cx("Find Template")} />
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </form>
+      <div className={cx('wrapper', { wrapper__sidebarOpen: sidebarOpen })}>
+        <div className={cx('core')}>
+          <div className={cx('theme-top')}>
+            <div className={cx('theme-title')}>
+              <p>Featured categories</p>
+              <form className={cx('theme-form')}>
+                <input placeholder={cx('Find Template')} />
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </form>
+            </div>
+            <ul className={cx('theme-list-categories')}>
+              {bckThemesIconData.map((item, index) => {
+                return (
+                  <Link
+                    key={index}
+                    to={`/b/${item.data.board.boardId}`}
+                    onClick={() => handleGetTemplate(item.data)}
+                  >
+                    <li>
+                      <img src={item.img} alt={item.title} />
+                      <span>{item.title}</span>
+                    </li>
+                  </Link>
+                );
+              })}
+            </ul>
           </div>
-          <ul className={cx("theme-list-categories")}>
-            {bckThemesIconData.map((item) => {
-              return (
-                <Link
-                  to={`/b/${item.data.board.boardId}`}
-                  onClick={() => handleGetTemplate(item.data)}
-                >
-                  <li>
-                    <img src={item.img} alt={item.title} />
-                    <span>{item.title}</span>
-                  </li>
-                </Link>
-              );
-            })}
-          </ul>
         </div>
       </div>
     </SidebarLayout>
